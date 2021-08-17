70f06d7b452fdd725b3fdb2871bec31c3f34c079
Bilwin <marf.zpd@gmail.com>
2021-08-17 22:52:44 +0900

    Инициализация

---
+++ code/Game.cs
@@ -0,0 +1,30 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Agency
+{
+	public partial class Agency : Sandbox.Game
+	{
+		public Agency()
+		{
+			if ( IsServer )
+			{
+				new AgencyHud();
+			}
+		}
+
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new AgencyPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+}
---
+++ code/UI/Hud.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox.UI;
+
+namespace Agency
+{
+	public partial class AgencyHud : Sandbox.HudEntity<RootPanel>
+	{
+		public AgencyHud()
+		{
+			if ( IsClient )
+			{
+				RootPanel.SetTemplate( "UI/hud.html" );
+			}
+		}
+	}
+}

ab787cae1d94fee7b0542c8e17998a71e2cb950a
Bilwin <marf.zpd@gmail.com>
2021-08-22 14:51:12 +0900

    Create Round.cs

---
+++ code/Round.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agency
+{
+	public class Round
+	{
+		public bool RoundStarted
+		{
+			get
+			{
+				return RoundStarted;
+			}
+
+			set
+			{
+				RoundStarted = value;
+				Event.Run("OnRoundStateChanged", value);
+			}
+		}
+	}
+}
